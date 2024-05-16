da9735668d8274f93d8a35f71eddee0dbc54e4a1
Mehul-Suryavanshi <mehulsuryawanshi9@gmail.com>
2024-05-16 16:24:42 +0530

    Some dev done

--- force-app/main/default/lwc/boatSearchResults/boatSearchResults.js
+++ force-app/main/default/lwc/boatSearchResults/boatSearchResults.js
@@ -1,3 +1,18 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire } from "lwc";
+import getBoats from "@salesforce/apex/BoatDataService.getBoats";
+export default class BoatSearchResults extends LightningElement {
+  boats = [];
+  error;
 
-export default class BoatSearchResults extends LightningElement {}
+  @wire(getBoats)
+  wiredBoats({ error, data }) {
+    if (data) {
+      this.boats = data;
+      console.log("CHECK :: " + JSON.stringify(this.boats));
+      this.error = undefined;
+    } else if (error) {
+      this.error = error;
+      this.boats = undefined;
+    }
+  }
+}
