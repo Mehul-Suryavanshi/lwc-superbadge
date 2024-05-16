f3a883f62b6c97d9fbdf8edae712b96f22e8e1d0
Mehul-Suryavanshi <mehulsuryawanshi9@gmail.com>
2024-05-16 17:34:58 +0530

    Completed gallery in boatSearchResult

--- force-app/main/default/lwc/boatSearchResults/boatSearchResults.js
+++ force-app/main/default/lwc/boatSearchResults/boatSearchResults.js
@@ -1,9 +1,23 @@
 import { LightningElement, wire } from "lwc";
 import getBoats from "@salesforce/apex/BoatDataService.getBoats";
+import BOATMC from "@salesforce/messageChannel/BoatMessageChannel__c";
+import { publish, MessageContext } from "lightning/messageService";
+const SUCCESS_TITLE = "Success";
+const MESSAGE_SHIP_IT = "Ship it!";
+const SUCCESS_VARIANT = "success";
+const ERROR_TITLE = "Error";
+const ERROR_VARIANT = "error";
 export default class BoatSearchResults extends LightningElement {
-  boats = [];
-  error;
+  selectedBoatId;
+  boatTypeId = "";
+  boats;
+  isLoading = false;
 
+  columns = [];
+  // wired message context
+  @wire(MessageContext)
+  messageContext;
+  // wired getBoats method
   @wire(getBoats)
   wiredBoats({ error, data }) {
     if (data) {
@@ -15,4 +29,43 @@ export default class BoatSearchResults extends LightningElement {
       this.boats = undefined;
     }
   }
+  // public function that updates the existing boatTypeId property
+  // uses notifyLoading
+  searchBoats(boatTypeId) {}
+
+  // this public function must refresh the boats asynchronously
+  // uses notifyLoading
+  refresh() {}
+
+  // this function must update selectedBoatId and call sendMessageService
+  updateSelectedTile(evt) {
+    console.log("SELECTED Boat Id :: " + evt.detail.boatId);
+    this.selectedBoatId = evt.detail.boatId;
+    this.sendMessageService(this.selectedBoatId);
+  }
+
+  // Publishes the selected boat Id on the BoatMC.
+  sendMessageService(boatId) {
+    publish(this.messageContext, BOATMC, {
+      recordId: boatId
+    });
+    // explicitly pass boatId to the parameter recordId
+  }
+
+  // The handleSave method must save the changes in the Boat Editor
+  // passing the updated fields from draftValues to the
+  // Apex method updateBoatList(Object data).
+  // Show a toast message with the title
+  // clear lightning-datatable draft values
+  handleSave(event) {
+    // notify loading
+    const updatedFields = event.detail.draftValues;
+    // Update the records via Apex
+    updateBoatList({ data: updatedFields })
+      .then(() => {})
+      .catch((error) => {})
+      .finally(() => {});
+  }
+  // Check the current value of isLoading before dispatching the doneloading or loading custom event
+  notifyLoading(isLoading) {}
 }
